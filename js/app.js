66c779471f9e28965945201df55d730b5082f1c6
daibh <oniwa1102@gmail.com>
2023-03-28 02:42:35 +0700

    Update app.js
    updated

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ var GoogleAnalysis = /** @class */ (function () {
         var event = el.getAttribute('gaEvent');
         var category = el.getAttribute('gaCategory');
         var value = el.getAttribute('gaValue');
-        window.dataLayer.push({ event, evenModel: { event, category, value } });
+        window.dataLayer.push({ event: 'click', evenModel: { event, category, value } });
       };
       for (var i = 0; i < links.length; i++) {
         var _link = links.item(i);
