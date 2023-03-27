96d02f52b28515f9afa303e669b5b47431bdf97f
daibh <oniwa1102@gmail.com>
2023-03-27 23:28:26 +0700

    Update app.js
    updated

--- js/app.js
+++ js/app.js
@@ -1,78 +1,80 @@
 var GoogleAnalysis = /** @class */ (function () {
   function GoogleAnalysis(code) {
-      this.events = [];
-      this.code = code;
-      this.injectDependences();
+    this.events = [];
+    this.code = code;
+    this.injectDependences();
   }
   GoogleAnalysis.init = function (code) {
-      console.log('GoogleAnalysis::initialize', { code: code });
-      if (!GoogleAnalysis.instance) {
-          return GoogleAnalysis.instance = new GoogleAnalysis(code);
-      }
-      return GoogleAnalysis.instance;
+    console.log('GoogleAnalysis::initialize', { code: code });
+    if (!GoogleAnalysis.instance) {
+      return GoogleAnalysis.instance = new GoogleAnalysis(code);
+    }
+    return GoogleAnalysis.instance;
   };
   GoogleAnalysis.prototype.injectDependences = function () {
-      // Google tag (gtag.js)
-      (function (d, s, id, c) {
-          var e, fsc = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {
-              return;
-          }
-          e = d.createElement(s);
-          e.id = id;
-          e.src = "https://www.googletagmanager.com/gtag/js?id=".concat(c);
-          e.setAttribute('async', true);
-          fsc.parentNode.insertBefore(e, fsc);
-      }(document, 'script', 'gtag-js-sdk', this.code));
-      window.dataLayer = window.dataLayer || [];
-      this.gtag('js', new Date());
-      this.gtag('config', 'G-DL3FM9GLQE');
+    // Google tag (gtag.js)
+    (function (d, s, id, c) {
+      var e, fsc = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      e = d.createElement(s);
+      e.id = id;
+      e.src = "https://www.googletagmanager.com/gtag/js?id=".concat(c);
+      e.setAttribute('async', true);
+      fsc.parentNode.insertBefore(e, fsc);
+    }(document, 'script', 'gtag-js-sdk', this.code));
+    window.dataLayer = window.dataLayer || [];
+    this.gtag('js', new Date());
+    this.gtag('config', 'G-DL3FM9GLQE');
   };
   GoogleAnalysis.prototype.getDefinedEvents = function () {
-      var _this = this;
-      var links = document.getElementsByTagName('a');
-      var _loop_1 = function (i) {
-          var _link = links.item(i);
-          _link.addEventListener('click', function (e) {
-              var event = _link.getAttribute('gaEvent');
-              var category = _link.getAttribute('gaCategory');
-              var value = _link.getAttribute('gaValue');
-              _this.gtag(event, category, value);
-          });
-      };
-      for (var i = 0; i < links.length; i++) {
-          _loop_1(i);
-      }
-      return [];
+    var _this = this;
+    var links = document.getElementsByTagName('a');
+    var _loop_1 = function (i) {
+      var _link = links.item(i);
+      _link.addEventListener('click', function (e) {
+        var event = _link.getAttribute('gaEvent');
+        var category = _link.getAttribute('gaCategory');
+        var value = _link.getAttribute('gaValue');
+        _this.gtag({ event, category, value });
+      });
+    };
+    for (var i = 0; i < links.length; i++) {
+      _loop_1(i);
+    }
+    return [];
   };
   GoogleAnalysis.prototype.addEvent = function (event) {
-      this.events.push(event);
+    this.events.push(event);
   };
   GoogleAnalysis.prototype.addEvents = function (events) {
-      var _a;
-      (_a = this.events).push.apply(_a, events);
+    var _a;
+    (_a = this.events).push.apply(_a, events);
   };
   GoogleAnalysis.prototype.bindEvents = function () {
-      var _this = this;
-      window.addEventListener('DOMContentLoaded', function () {
-          _this.getDefinedEvents();
-          _this.events.forEach(function (_a) {
-              var name = _a.name, listener = _a.listener;
-              return window.document.addEventListener(name, listener);
-          });
+    var _this = this;
+    window.addEventListener('DOMContentLoaded', function () {
+      _this.getDefinedEvents();
+      _this.events.forEach(function (_a) {
+        var name = _a.name, listener = _a.listener;
+        return window.document.addEventListener(name, listener);
       });
-      window.addEventListener('beforeunload', function () { return _this.events.forEach(function (_a) {
-          var name = _a.name, listener = _a.listener;
-          return window.document.removeEventListener(name, listener);
-      }); });
+    });
+    window.addEventListener('beforeunload', function () {
+      return _this.events.forEach(function (_a) {
+        var name = _a.name, listener = _a.listener;
+        return window.document.removeEventListener(name, listener);
+      });
+    });
   };
   GoogleAnalysis.prototype.gtag = function () {
-      var params = [];
-      for (var _i = 0; _i < arguments.length; _i++) {
-          params[_i] = arguments[_i];
-      }
-      params.forEach(function (_) { return console.log('gtag', _); });
-      window.dataLayer.push(params);
+    var params = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+      params[_i] = arguments[_i];
+    }
+    params.forEach(function (_) { return console.log('gtag', _); });
+    window.dataLayer.push(params);
   };
   return GoogleAnalysis;
 }());
@@ -80,7 +82,7 @@ var googleAnalysis = GoogleAnalysis.init('G-DL3FM9GLQE');
 googleAnalysis.addEvent({
   name: 'click',
   listener: function (e) {
-      console.log('click', e, this);
+    console.log('click', e, this);
   }
 });
 googleAnalysis.bindEvents();
